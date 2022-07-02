13ea3eb195a82ec8fdca450c76bd3d1a6b192af3
shadyashraf174 <97650533+shadyashraf174@users.noreply.github.com>
2022-07-02 04:14:26 +0300

    Update java1.java

--- java1.java
+++ java1.java
@@ -1,13 +1,15 @@
 public class first {
   public static void main(String[] args) {
-    System.out.println(" SSS    A   A");
-    System.out.println("S   S   A   A");
-    System.out.println("S       A   A");
-    System.out.println(" SSS    AAAAA");
-    System.out.println("    S   A   A");
-    System.out.println("S   S   A   A");
-    System.out.println(" SSS    A   A");
-    
+    System.out.println(" _______________________________________");
+    System.out.println("|                                       |");
+    System.out.println("|  SSS    H   H     A     DDDD    Y   Y |");
+    System.out.println("| S   S   H   H    A A    D   D   Y   Y |");
+    System.out.println("| S       H   H   A   A   D   D    Y Y  |");
+    System.out.println("|  SSS    HHHHH   AAAAA   D   D     Y   |");
+    System.out.println("|     S   H   H   A   A   D   D     Y   |");
+    System.out.println("| S   S   H   H   A   A   D   D     Y   |");
+    System.out.println("|  SSS    H   H   A   A   DDDD      Y   |");
+    System.out.println("|_______________________________________|");
   }
-  
+
 }
