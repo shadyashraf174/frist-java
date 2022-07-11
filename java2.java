180f007471d7d2d2d5e8692f3033709493422979
shadyashraf174 <97650533+shadyashraf174@users.noreply.github.com>
2022-07-11 07:17:27 +0300

    Update java2.java

--- java2.java
+++ java2.java
@@ -1,15 +1,18 @@
-public class first {
-  public static void main(String[] args) {
-    System.out.println(" _______________________________________");
-    System.out.println("|                                       |");
-    System.out.println("|  SSS    H   H     A     DDDD    Y   Y |");
-    System.out.println("| S   S   H   H    A A    D   D   Y   Y |");
-    System.out.println("| S       H   H   A   A   D   D    Y Y  |");
-    System.out.println("|  SSS    HHHHH   AAAAA   D   D     Y   |");
-    System.out.println("|     S   H   H   A   A   D   D     Y   |");
-    System.out.println("| S   S   H   H   A   A   D   D     Y   |");
-    System.out.println("|  SSS    H   H   A   A   DDDD      Y   |");
-    System.out.println("|_______________________________________|");
-  }
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "shady";
+        int age = 21 ;
+        String country = "Egypt";
+        String sport = "fatbool";
+        int hours = 2;
+        String game = "video_game";
+        String subjectt = "math";
+        char grade = 'b';
 
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from "+country+". ");
+        System.out.println("My favourite sport is "+sport+". I play for "+hours+" hours a day");
+        System.out.println("When I'm tired, I like to play "+ game+".");
+        System.out.println("In school, my favourite subject was "+subjectt+", I scored a "+grade+".");
+
+    }
 }
